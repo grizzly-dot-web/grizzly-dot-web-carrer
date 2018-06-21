00fd49d83805b9715fdac462d333a10e3e9c0859
Sebastian Müller <s.mueller@sebgrizzly.de>
2018-06-21 14:58:54 +0200

    :sparkles: :lipstick: header styling

--- src/frontend/ts/App/Header.tsx
+++ src/frontend/ts/App/Header.tsx
@@ -14,10 +14,39 @@ class Header extends React.Component<AppProps> {
 	}
 
 	render() {
+		let spanPerCharacter = (word : string) => {
+			let chars = [];
+			let counter = 0;
+			for (let char of word) {
+				counter++;
+				chars.push(
+					<span key={counter}>{char}</span>
+				);
+			}
+
+			return (<div className={"justify-chars"}>{chars}</div>);
+		}
 
 		return (
-			<header>
+			<header id="page-header" className={"header__right-dark"}>
 				<div className="container">
+					<a className="logo" href="/">
+						<h1>
+							Sebastian<br />
+							Müller
+						</h1>
+						<h2>
+							{spanPerCharacter('grizzly.web')}
+						</h2>
+					</a>
+					<a className="experience-link">
+						Skills <span className="circle">&</span> Referenzen
+					</a>
+					<nav className="main-nav">
+						<a href="/">Start</a>
+						<a href="/career">Karriere</a>
+						<a href="/booking">Beauftragen</a>
+					</nav>
 				</div>
 			</header>
 		);
