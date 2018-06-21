a81043c0f6e91f31065756e3eb630c55f114bf20
Sebastian Müller <s.mueller@sebgrizzly.de>
2018-06-21 17:31:40 +0200

    :lipstick: footer styling

--- src/frontend/ts/App/Footer.tsx
+++ src/frontend/ts/App/Footer.tsx
@@ -13,19 +13,22 @@ class Header extends React.Component<AppProps> {
 		let socialLinks = [];
 		for (let link of this.props.data.social_links) {
 			socialLinks.push((
-				<a key={link.url} target="_blank" href={link.url}><img src={link.icon} alt=""/></a>
+				<a key={link.url} target="_blank" href={link.url}><img src={link.icon} alt=""/>{link.name}</a>
 			));
 		}
 
 		return (
 			<footer>
-				<div className="profile">
-					<div className="container">
-						<div className="social-links">
-							<h4>Zu finden bei</h4>
-							{socialLinks}
-						</div>
+				<div className="container">
+					<div className="social-links">
+						<h4>Du findest mich auch auf</h4>
+						{socialLinks}
 					</div>
+					<nav className="meta-nav">
+						<h4>Zu dieser Seite</h4>
+						<a href="/feedback">Feedback / Bug report</a>
+						<a href="https://github.com/sebgrizzly/LifeCareer" target="_blank">Opensource (github.com)</a>
+					</nav>
 				</div>
 			</footer>
 		);
